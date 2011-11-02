6b0a45ab6272e5d5dc9337d0238b0dcfc4ea5325
eri451 <hans.orter@gmx.de>
2011-11-02 01:56:53 +0100

    json for saving

--- fridge.py
+++ fridge.py
@@ -1,4 +1,16 @@
-fridge ={}
+import json
+
+def load():
+    try:
+        f = open("fridge.json","r")
+        fridge = json.load(f)
+        f.close()
+    except:
+        fridge = {}
+    return fridge
+
+fridge = load()
+
 
 def open_fridge(product, action, amount):
     if (not(fridge.has_key(product)) and action != sub):
@@ -8,24 +20,30 @@ def open_fridge(product, action, amount):
         if action == "add":
             for i in range(amount):
                 add_product(product)
+            save(fridge)
         elif action == "sub":
             for i in range(amount):
                 sub_product(product)
+            save(fridge)
         else:
             return "Bitte sub oder add ein Produkt!"
 
-
 def add_product(product):
     fridge[product] += 1
-    message = "Produkt hinzugefuegt"
+    message = product + " hinzugefuegt"
     return message
 
 def sub_product(product):
     if fridge[product] >= 1:
         fridge[product] -= 1
-        message = "Produkt rausgenommen"
+        message = produkt + "rausgenommen"
     else:
         message = "Fridge leer!"
     return message
 
-
+def save(fridge):
+    json.dump(fridge, jfridge)
+    f = open("fridge.json", "w")
+    f.write(jfridge)
+    f.close()
+    return "saved"
