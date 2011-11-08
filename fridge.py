b820e6efe879f5cce051b1dd098145c11b091955
eri451 <hans.orter@gmx.de>
2011-11-08 11:08:02 +0100

    show Fridgecontent

--- fridge.py
+++ fridge.py
@@ -12,15 +12,17 @@ def main(args):
     if len(args) <= 1:
         action  = raw_input("Operator = ")
         if action == "show":
-            #TODO print fridge.
-            print "Yes we know, this is a TODO"
+            show_fridge()
             return
-        product = raw_input("Produkt = ")
         amount  = int(raw_input("Anzahl = "))
+        product = raw_input("Produkt = ")
+    elif args[1] == "show":
+        show_fridge()
+        return
     else:
         action  = args[1]
-        product = args[2]
-        amount = int(args[3])
+        amount = int(args[2])
+        product = args[3]
     open_fridge(action, product, amount)
 
 def open_fridge(action, product, amount):
@@ -30,12 +32,21 @@ def open_fridge(action, product, amount):
         print str(amount) + " " + product + " hinzugefuegt"
         save(fridge)
     elif action == "sub":
+        if (not(fridge.has_key(product))):
+            print "erst reinlegen dann raus nehmen!!"
+            return
         for i in range(amount):
             sub_product(product)
         print str(amount) + " " + product + "rausgenommen"
+        print str(fridge[product]) + " " + product + "verbleiben"
         save(fridge)
     else:
          print "falsche Eingabe. Bitte nutze \"show\", \"add\" oder \"sub\"."
+
+def show_fridge():
+    for product in fridge.keys():
+        print "" + product + " " + str(fridge[product])
+
 
 def add_product(product):
     if (not(fridge.has_key(product))):
